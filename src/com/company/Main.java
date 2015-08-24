a430c8e8ccae60351636a1f862b6d005dd0aabb9
dsandeephegde <sandeeph@thoughtworks.com>
2015-08-24 17:36:04 +0530

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        //first change
+        Scanner sc = new Scanner(System.in);
+
+
     }
 }
