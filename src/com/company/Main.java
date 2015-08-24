db07366d7e2fbb8640c25115ab6d70e49c52eb38
dsandeephegde <sandeeph@thoughtworks.com>
2015-08-24 17:49:04 +0530

    commit 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,11 +9,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n[] = new int[50];
         double p[] = new double[50];
-
+        String s[] = new String[50];
         for(int i=0;sc.next()!=null;i++)
         {
             n[i]= sc.nextInt();
-
+            s[i]= sc.next();
             p[i]=sc.nextDouble();
         }
 
