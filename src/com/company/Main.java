5b3799cb857af7ee831a6ea9c3da9708aefc8fd0
dsandeephegde <sandeeph@thoughtworks.com>
2015-08-24 17:51:55 +0530

    commit 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,12 +10,20 @@ public class Main {
         int n[] = new int[50];
         double p[] = new double[50];
         String s[] = new String[50];
+        int num=0;
         for(int i=0;sc.next()!=null;i++)
         {
             n[i]= sc.nextInt();
             s[i]= sc.next();
             p[i]=sc.nextDouble();
+            num=i;
         }
 
+        for(int i=0;i<num;i++)
+        {
+            System.out.print(n[i]);
+            System.out.print(s[i]);
+            System.out.print(p[i]);
+        }
     }
 }
